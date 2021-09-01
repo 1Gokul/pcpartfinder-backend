0a93222f480167c43abb8be840d7b048870c058d
1Gokul <viswanath1gokul@gmail.com>
2021-09-01 11:43:59 +0530

    Added RP Tech

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from starlette.responses import RedirectResponse
 import asyncio
 
-from scraper.components import md_computers, prime_abgb, vedant_computers
+from scraper.components import md_computers, prime_abgb, rp_tech, vedant_computers
 
 description = """
 The PCPartFinder API scrapes and finds information about the availability of different PC components in India.
@@ -15,10 +15,9 @@ The PCPartFinder API scrapes and finds information about the availability of dif
 - Prime ABGB
 """
 
-tags_metadata = [{
-    "name": "search",
-    "description": "Search and scrape information from the sources."
-}]
+tags_metadata = [
+    {"name": "search", "description": "Search and scrape information from the sources."}
+]
 
 app = FastAPI(
     title="PCPartFinder",
@@ -32,8 +31,8 @@ app = FastAPI(
     openapi_tags=tags_metadata,
     license_info={
         "name": "GNU GPL 3.0",
-        "url": "https://github.com/1Gokul/pcpartfinder-backend/blob/main/LICENSE"
-    }
+        "url": "https://github.com/1Gokul/pcpartfinder-backend/blob/main/LICENSE",
+    },
 )
 
 
@@ -41,11 +40,17 @@ app = FastAPI(
 async def root():
     return RedirectResponse(url="/docs")
 
+
 @app.get("/api/search/{search_query}", tags=["search"])
 async def search(search_query: str):
     if not search_query:
         raise HTTPException(status_code=400, detail="Search query not provided.")
     else:
-        functions = [vedant_computers(search_query), md_computers(search_query), prime_abgb(search_query)]
+        functions = [
+            vedant_computers(search_query),
+            md_computers(search_query),
+            prime_abgb(search_query),
+            rp_tech(search_query),
+        ]
         search_results = await asyncio.gather(*functions)
         return search_results
--- app/scraper/components.py
+++ app/scraper/components.py
@@ -5,16 +5,20 @@ headers = {
     "User-Agent": "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
 }
 
+
 def get_soup(link):
     response = requests.get(
         link,
         headers=headers,
     )
     print(link)
-    return BeautifulSoup(response.content, "lxml")    
+    return BeautifulSoup(response.content, "lxml")
+
 
 async def vedant_computers(query):
-    soup = get_soup(f"https://www.vedantcomputers.com/index.php?route=product/search&search={query}")
+    soup = get_soup(
+        f"https://www.vedantcomputers.com/index.php?route=product/search&search={query}"
+    )
 
     results = []
     for product in soup.select(".main-products.product-grid .caption"):
@@ -26,14 +30,14 @@ async def vedant_computers(query):
             }
         )
 
-    return {
-        "site": "vedant_computers",
-        "results": results
-    }
+    return {"site": "vedant_computers", "results": results}
+
 
 async def md_computers(query):
     search_query = query.replace(" ", "+")
-    soup = get_soup(f"https://mdcomputers.in/index.php?search={search_query}&submit_search=&route=product%2Fsearch")
+    soup = get_soup(
+        f"https://mdcomputers.in/index.php?search={search_query}&submit_search=&route=product%2Fsearch"
+    )
 
     results = []
     for product in soup.select(".product-item-container .right-block.right-b"):
@@ -45,14 +49,14 @@ async def md_computers(query):
             }
         )
 
-    return {
-        "site": "md_computers",
-        "results": results
-    }
+    return {"site": "md_computers", "results": results}
+
 
 async def prime_abgb(query):
     search_query = query.replace(" ", "+")
-    soup = get_soup(f"https://www.primeabgb.com/?post_type=product&taxonomy=product_cat&product_cat=0&s={search_query}")
+    soup = get_soup(
+        f"https://www.primeabgb.com/?post_type=product&taxonomy=product_cat&product_cat=0&s={search_query}"
+    )
 
     results = []
     for product in soup.select(".products .product-innfo"):
@@ -60,13 +64,29 @@ async def prime_abgb(query):
             {
                 "name": product.select_one(".product-name a").get_text(),
                 "link": product.select_one(".product-name a")["href"],
-                "price": product.select_one(".woocommerce-Price-amount").get_text(strip=True),
+                "price": product.select_one(".woocommerce-Price-amount").get_text(
+                    strip=True
+                ),
             }
         )
 
-    return {
-        "site": "prime_abgb",
-        "results": results
-    }
+    return {"site": "prime_abgb", "results": results}
 
 
+async def rp_tech(query):
+    search_query = query.replace(" ", "+")
+    soup = get_soup(
+        f"https://rptechindia.in/catalogsearch/result/index/?q={search_query}&stock_filter=258"
+    )
+
+    results = []
+    for product in soup.select(".products-row .product-box"):
+        results.append(
+            {
+                "name": product.select_one(".product-title a").get_text(strip=True),
+                "link": product.select_one(".product-title a")["href"],
+                "price": product.select_one(".price-tag").get_text(strip=True),
+            }
+        )
+
+    return {"site": "rp_tech", "results": results}
